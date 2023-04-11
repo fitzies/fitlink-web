0202dc988241b5c3440939bbd872f73ee80b3442
oli <olifitz1610@gmail.com>
2023-04-12 03:19:37 +0800

    fixed button hover bug

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -6,7 +6,7 @@ export default function Button(props: {
   if (props.type === "solid") {
     return (
       <div
-        className="flex justify-center items-center lg:w-32 lg:h-11 bg-black rounded-2xl hover:-translate-y-1 duration-150 cursor-pointer lg:p-0 p-2"
+        className="flex justify-center items-center lg:w-32 lg:h-11 bg-black rounded-2xl lg:hover:-translate-y-1 duration-150 cursor-pointer lg:p-0 p-2"
         onClick={() => props.onClick()}
       >
         <h1 className="text-white font-inter font-bold lg:text-lg text-md">
@@ -17,7 +17,7 @@ export default function Button(props: {
   } else if (props.type === "hollow") {
     return (
       <div
-        className="flex justify-center items-center lg:w-40 lg:h-11 border-2 border-black rounded-2xl hover:-translate-y-1 duration-150 cursor-pointer lg:p-0 p-2"
+        className="flex justify-center items-center lg:w-40 lg:h-11 border-2 border-black rounded-2xl lg:hover:-translate-y-1 duration-150 cursor-pointer lg:p-0 p-2"
         onClick={() => props.onClick()}
       >
         <h1 className="text-black font-inter lg:text-lg text-md">
